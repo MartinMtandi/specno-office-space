b271625b36aeb06857df28a1be0939368a043008
MartinMtandi <mtandimartin@gmail.com>
2025-01-31 10:33:18 +0200

    feat(issue 001): Card Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
 import styled from 'styled-components'
 import { Typography } from './components/Typography'
+import { Card } from './components/Card'
+
+function App() {
+  return (
+    <Wrapper>
+      <Container>
+        <Typography variant="h1">All Offices</Typography>
+        <Card
+          companyName="Specno"
+          totalStaff={45}
+          officeCapacity={50}
+          phoneNumber="+27 82 123 4567"
+          emailAddress="hello@specno.com"
+          companyAddress="123 Innovation Drive, Tech Park, Cape Town"
+          accent="#3B82F6"
+        />
+      </Container>
+    </Wrapper>
+  )
+}
 
 const Wrapper = styled.div`
   width: 100%;
@@ -16,22 +36,11 @@ const Container = styled.div`
   background-color: #F8FAFC;
   display: flex;
   flex-direction: column;
+  gap: 24px;
+  min-height: calc(100vh - 16px);
   padding: 16px;
   box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.1);
   box-sizing: border-box;
 `
-
-function App() {
-  return (
-    <Wrapper>
-      <Container>
-        <Typography variant="h1">Welcome to Office Space</Typography>
-        <Typography variant="body">
-          Manage your office space efficiently
-        </Typography>
-      </Container>
-    </Wrapper>
-  )
-}
 
 export default App
