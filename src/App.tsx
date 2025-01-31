4667fcab424a78bb55fc2adbe8b8492b159ef9c4
MartinMtandi <mtandimartin@gmail.com>
2025-01-31 11:52:32 +0200

    feat(Issue02): Floating button

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,19 @@
-import styled from 'styled-components'
-import { Typography } from './components/Typography'
-import { Card } from './components/Card'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Office } from './pages/Office'
+import { Home } from './pages/Home'
+import { MainLayout } from './layouts/MainLayout'
 
 function App() {
   return (
-    <Wrapper>
-      <Container>
-        <Typography variant="h1">All Offices</Typography>
-        <Card
-          companyName="Specno"
-          totalStaff={45}
-          officeCapacity={50}
-          phoneNumber="+27 82 123 4567"
-          emailAddress="hello@specno.com"
-          companyAddress="123 Innovation Drive, Tech Park, Cape Town"
-          accent="#3B82F6"
-        />
-      </Container>
-    </Wrapper>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/office" element={<Office />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
-
-const Wrapper = styled.div`
-  width: 100%;
-  background-color: #FFF;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  box-sizing: border-box;
-  overflow-y: hidden;
-`
-
-const Container = styled.div`
-  max-width: 480px;
-  background-color: #F8FAFC;
-  display: flex;
-  flex-direction: column;
-  gap: 24px;
-  min-height: calc(100vh - 16px);
-  padding: 16px;
-  box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.1);
-  box-sizing: border-box;
-`
 
 export default App
---
+++ src/pages/Office.tsx
@@ -0,0 +1,7 @@
+export const Office = () => {
+  return (
+    <>
+      Office Page
+    </>
+  )
+}
